2eb5e17451444ac93a762063f9f82bfff211ee26
Johanne Marie Dehn Nielsen <jon010@edu.zealand.dk>
2025-10-07 10:54:39 +0200

    function appendToList is implemnted and tages 2 parameters listId and liElement

--- js/index.js
+++ js/index.js
@@ -1,6 +1,9 @@
 //Write your Javascript code here
 console.log("Shoppinglist")
 
+//Create a function that return a new Li element
+//The function should have at least three parameters, one for the class attribute, 
+// one for the id attribute and one for the text itself in the “li” element.
 function newelement(id,clas,text)
 {
     let newEl = document.createElement("li")
@@ -11,4 +14,16 @@ function newelement(id,clas,text)
     return newEl
 }
 
-newelement("Seven","unhelthy","White chocklate")
+let el = newelement("Seven","unhelthy","White chocklate")
+
+//Create a function that append a Li element to a list
+//The function should have two parameters one for the id of the list 
+//and one for li element
+
+function appendToList(listId,liElement)
+{
+    let list = document.getElementById(listId)
+    list.append(liElement)
+}
+
+appendToList("list",el)
