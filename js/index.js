1a3bcd03ee5f39e9fbb5886c4b6f6527aa721bc8
Johanne Marie Dehn Nielsen <jon010@edu.zealand.dk>
2025-10-07 10:43:37 +0200

    Function newelement added in indexJs. Function returns an element

--- js/index.js
+++ js/index.js
@@ -1,2 +1,14 @@
 //Write your Javascript code here
 console.log("Shoppinglist")
+
+function newelement(id,clas,text)
+{
+    let newEl = document.createElement("li")
+    newEl.setAttribute("id",id)
+    newEl.setAttribute("Class",clas)
+    let txt = document.createTextNode(text)
+    newEl.append(txt)
+    return newEl
+}
+
+newelement("Seven","unhelthy","White chocklate")
